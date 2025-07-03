4161d3e1e4eec989a879ff4165730bbff34194af
blacksnowx <jenks.tyler@gmail.com>
2025-07-03 17:40:49 -0400

    updated docker-entrypoint.sh for better initialization

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,6 @@ set -e
 # Run the database initialization script
 echo "Running database initialization..."
 python init_db.py
-
+echo "Database initialization complete."
 # Then, execute the main command passed to the script (e.g., uvicorn)
 exec "$@"
